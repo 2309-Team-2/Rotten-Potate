0b284f468cbcfec1308e9002c19306ad0e268304
Almightyjuju <alexanderpizzati11@gmail.com>
2023-12-06 12:44:24 -0600

    added timestamps organized files

--- src/server/db/reviews.js
+++ src/server/db/reviews.js
@@ -1,23 +1,28 @@
-const db = require('./client'); // Adjust the path as necessary to point to your database client setup.
+const db = require("./client"); // Adjust the path as necessary to point to your database client setup.
 
 // Function to get all reviews
 async function getAllReviews() {
-  const result = await db.query('SELECT * FROM reviews');
+  const result = await db.query("SELECT * FROM reviews");
   return result.rows;
 }
 
 // Function to get a single review by ID
 async function getReviewById(id) {
-  const result = await db.query('SELECT * FROM reviews WHERE id = $1', [id]);
+  const result = await db.query("SELECT * FROM reviews WHERE id = $1", [id]);
   return result.rows[0]; // Return the first row (review) or undefined if not found
 }
 
 // Function to create a new review
 async function createReview(reviewData) {
   const { users_id, movie_id, rating, comment } = reviewData;
+  const createdAt = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   const result = await db.query(
-    'INSERT INTO reviews (users_id, movie_id, rating, comment) VALUES ($1, $2, $3, $4) RETURNING *',
-    [users_id, movie_id, rating, comment]
+    "INSERT INTO reviews (users_id, movie_id, rating, comment, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [users_id, movie_id, rating, comment, createdAt]
   );
   return result.rows[0]; // Return the new review
 }
@@ -25,43 +30,50 @@ async function createReview(reviewData) {
 // Function to update a review
 async function updateReview(id, reviewData) {
   const { rating, comment } = reviewData;
+  const updatedAt = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   const result = await db.query(
-    'UPDATE reviews SET rating = $2, comment = $3 WHERE id = $1 RETURNING *',
-    [id, rating, comment]
+    "UPDATE reviews SET rating = $2, comment = $3 , updated_at = $4 WHERE id = $1 RETURNING *",
+    [id, rating, comment, updatedAt]
   );
   return result.rows[0]; // Return the updated review
 }
 
 // Function to delete a review
 async function deleteReview(id) {
-  const result = await db.query('DELETE FROM reviews WHERE id = $1 RETURNING *', [id]);
+  const result = await db.query(
+    "DELETE FROM reviews WHERE id = $1 RETURNING *",
+    [id]
+  );
   return result.rows[0]; // Return the deleted review
 }
 
 async function getReviewsByMovieId(movieId) {
   try {
-      const query = 'SELECT * FROM reviews WHERE movie_id = $1';
-      const values = [movieId];
+    const query = "SELECT * FROM reviews WHERE movie_id = $1";
+    const values = [movieId];
 
-      const result = await db.query(query, values);
-      return result.rows;
+    const result = await db.query(query, values);
+    return result.rows;
   } catch (error) {
-      throw error;
+    throw error;
   }
 }
 
 async function deleteReviewsByMovieId(movieId) {
   try {
-      const query = 'DELETE FROM reviews WHERE movie_id = $1';
-      const values = [movieId];
+    const query = "DELETE FROM reviews WHERE movie_id = $1";
+    const values = [movieId];
 
-      const result = await db.query(query, values);
-      return result.rowCount; // Returns the number of deleted rows
+    const result = await db.query(query, values);
+    return result.rowCount; // Returns the number of deleted rows
   } catch (error) {
-      throw error;
+    throw error;
   }
 }
-
 
 module.exports = {
   getAllReviews,
@@ -70,5 +82,5 @@ module.exports = {
   updateReview,
   deleteReview,
   getReviewsByMovieId,
-  deleteReviewsByMovieId
+  deleteReviewsByMovieId,
 };
