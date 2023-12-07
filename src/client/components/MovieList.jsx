526dddd51a10d218f3a6311120a743f1e4ea5287
tristan <tsa1431@DESKTOP-9J69REN>
2023-12-06 19:06:56 -0600

    finally got genres to filter

--- src/client/components/MovieList.jsx
+++ src/client/components/MovieList.jsx
@@ -33,38 +33,42 @@ function MovieList() {
 
   const handleFilterChange = async (selectedCategory) => {
     try {
+      console.log('Selected Category:', selectedCategory);
+  
       const response = await fetch(`/api/genres?category=${selectedCategory}`);
       if (!response.ok) {
         throw new Error(`Failed to fetch movies. Status: ${response.status}`);
       }
-
+  
       const moviesData = await response.json();
+      console.log('Filtered Movies:', moviesData);
+  
       setFilteredMovies(moviesData);
     } catch (error) {
       console.error('Error fetching movies:', error.message);
     }
   };
   
-  return (
-    <div>
-      <h2 className="movies-list-title">Movies List</h2>
-      <CategoryFilter onFilterChange={handleFilterChange} />
-      <ul className='all-movies-list'>
-        {movies.map((movie) => (
-          <li key={movie.id} className="movie-item">
-            <Link to={`/movies/${movie.id}`} className="movie-link">
-              <img src={movie.imageUrl} alt={movie.title} className="movie-image" />
-              <h3>{movie.title}</h3>
-              {/* Display the movie image */}
-              <p>Release Year: {movie.release_year}</p>
-              <p>Rating: {movie.rating}</p>
-              {/* Add more details as needed */}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+return (
+  <div>
+    <h2 className="movies-list-title">Movies List</h2>
+    <CategoryFilter onFilterChange={handleFilterChange} />
+    <ul className='all-movies-list'>
+  {filteredMovies.map((movie, index) => (
+    <li key={movie.id || index} className="movie-item">
+      <Link to={`/movies/${movie.id}`} className="movie-link">
+        <img src={movie.imageUrl} alt={movie.title} className="movie-image" />
+        <h3>{movie.title}</h3>
+        {/* Display the movie image */}
+        <p>Release Year: {movie.releaseYear}</p>
+        <p>Rating: {movie.rating}</p>
+        {/* Add more details as needed */}
+      </Link>
+    </li>
+  ))}
+</ul>
+  </div>
+);
 }
 
 export default MovieList;
