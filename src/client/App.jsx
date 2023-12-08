fd84bf684dde7a129caf678bb51ad49dfb48d03e
tristan <tsa1431@DESKTOP-9J69REN>
2023-12-08 14:10:24 -0600

    finished search bar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,47 +1,37 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Navigations from './components/Navigations';
 import Home from './components/Home';
-import AdminDashboard from './components/AdminDashboard'
-import CommentSection from './components/CommentSection'
-import MovieDetail from './components/MovieDetail'
-import Register from './components/Register'
-import ReviewList from './components/ReviewList'
-import SearchBar from './components/SearchBar'
-import SingleMovie from './components/SingleMovie'
+import AdminDashboard from './components/AdminDashboard';
+import CommentSection from './components/CommentSection';
+import MovieDetail from './components/MovieDetail';
+import Register from './components/Register';
+import ReviewList from './components/ReviewList';
+import SearchBar from './components/SearchBar';
+import SingleMovie from './components/SingleMovie';
 import Profile from './components/Profile';
 import MovieList from './components/MovieList';
-import RandomMovie from './components/RandomMovie'
-
+import RandomMovie from './components/RandomMovie';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [token, setToken] = useState(null);
-  const [user, setUser] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
-  
+
   const handleSearch = (data) => {
     setSearchResults(data);
   };
 
-
-
   return (
     <Router>
       <>
-
         <div className="header-container">
           <Link to="/" style={{ textDecoration: "none" }}>
             <h1 className="title">
-              <img id="logo-img" src="./holderlogo.png"></img>
+              <img id="logo-img" src="./holderlogo.png" alt="logo" />
               Rotten Potatoes
             </h1>
           </Link>
-            {/* Search Bar */}
-            <div className='search-bar-container'>
-            <SearchBar onSearch={handleSearch} />
-            </div>
           {token ? (
             // If the user is logged in, display the profile link and a logout button
             <div>
@@ -60,8 +50,7 @@ function App() {
               </Link>
             </div>
           )}
-          </div>
-        
+        </div>
 
         <Navigations />
 
@@ -73,7 +62,7 @@ function App() {
             element={<Profile token={token} setToken={setToken} />}
           />
           <Route path="/register" element={<Register />} />
-          <Route path="/allmovies" element={<MovieList />} />
+          <Route path="/allmovies" element={<MovieList searchResults={searchResults} />} />
           <Route path="/search" element={<MovieList searchResults={searchResults} />} />
           <Route path="/movies/:movieId" element={<MovieDetail />} />
           <Route path="/random" element={<RandomMovie />} />
