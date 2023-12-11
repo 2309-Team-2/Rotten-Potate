0d3d2976198d7d4655e18c7882889f843a6b2abc
Cameron Dixon <cam@camerons-mbp.mynetworksettings.com>
2023-12-11 14:12:22 -0500

    created a delete function that is now working

--- src/server/db/users.js
+++ src/server/db/users.js
@@ -102,18 +102,18 @@ const getUserByEmail = async (email) => {
 
 // Assuming you are using a SQL database and a library like `pg` for PostgreSQL
 async function deleteUser(userId) {
-  const client = await pool.connect();
   try {
-      await client.query('BEGIN');
-      const deleteQuery = 'DELETE FROM users WHERE id = $1';
-      const res = await client.query(deleteQuery, [userId]);
-      await client.query('COMMIT');
-      return res.rowCount; 
-  } catch (e) {
-      await client.query('ROLLBACK');
-      throw e;
-  } finally {
-      client.release();
+    const {
+      rows: [user],
+    } = await db.query('DELETE FROM users WHERE id = $1 RETURNING *', [userId]);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  } catch (err) {
+    throw err;
   }
 }
 
